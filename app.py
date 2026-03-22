3b5b2bd2e497060de1df7c14523fdf23840f84ee
M Harshitha <harshithareddym2007@gmail.com>
2026-03-22 22:11:04 +0530

    final port fix

--- app.py
+++ app.py
@@ -169,4 +169,5 @@ def edit_entry(id):
     return render_template('edit_entry.html', entry=entry)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
